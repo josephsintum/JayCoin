0a76b1737d1da27f54aa99a9b34c472e1641e785
sjumbam15 <sjumbam15@gmail.com>
2018-03-11 15:18:00 +0100

    blockchain.push new block

--- main.js
+++ main.js
@@ -13,3 +13,31 @@ class Block {
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
+
+class Blockchain {
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "11/03/2018", "Genesis Block", "0");
+    }
+
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    addBlock(newBlock) {
+        newBlock.previousHash = this.getLatestBlock().hash;
+        newBlock.hash = newBlock.calculateHash();
+        this.chain.push(newBlock);
+    }
+}
+
+// testing
+
+let JayCoin = new Blockchain();
+JayCoin.addBlock(new Block(1, "12/03/2018", { amount: 4}));
+JayCoin.addBlock(new Block(1, "14/03/2018", { amount: 12}));
+
+console.log(JSON.stringify(JayCoin, null, 4));
